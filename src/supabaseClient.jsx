730fe8c263601a7455194ad03bbd9dfa5760a03a
Rei-nk <wahyureihan20@gmail.com>
2026-03-30 00:44:36 +0700

    Fixing supabase import and casing final

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -7,4 +7,4 @@ const supabaseUrl = 'https://ohmwfkfqxqalbafiwham.supabase.co'
 // PASTE KUNCI FULL-NYA DI SINI (Pencet tombol Copy di web Supabase)
 const supabaseKey = 'sb_publishable_ddo9VLtZDd9A2yWt2aryzQ_bkfI...' 
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
